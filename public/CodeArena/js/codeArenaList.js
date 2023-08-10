206bbeb9e456e4363d6bfe51ffd3d741a1539eef
JIHUN <asdfgl98@naver.com>
2023-08-10 14:49:32 +0900

    이벤트 버블링 중간 저장

--- public/CodeArena/js/codeArenaList.js
+++ public/CodeArena/js/codeArenaList.js
@@ -73,11 +73,7 @@ const handleRoomSubmit = (event) => {
       nickname: res.data, // 사용자 이름
     });
     
-    arenaSocket.on("user_count", ({ user_count }) => {
-        console.log("user_count 이벤트 도착"), user_count;
-        $c_content_num.textContent = `${user_count}/4`;
-        $mini_room_users.textContent = `${user_count}/4`;
-      });
+    
       closeModal(); // 모달 닫고
       openarena(); // 방 입장
       arenaSocket.emit("welcome", { nickname: res.data });
@@ -89,13 +85,26 @@ const handleRoomSubmit = (event) => {
   $c_a_u_r_name2.textContent = room_name // Arena 제한 시간 위 방제
 };
 
+arenaSocket.on("user_count", ({ user_count }) => {
+  console.log("user_count 이벤트 도착"), user_count;
+  $c_content_num.textContent = `${user_count}/4`;
+  $mini_room_users.textContent = `${user_count}/4`;
+});
+
 // 방목록 최신화 ------------------지훈---------------------
+
+const clickEvent = ()=>{
+  
+}
+
 
 let currentNickname = "";
 let roomName
 let roomNum
 let roomLinks
+let clickEventHandler = null;
 //최신화 함수
+
 const updateArenaRoom = (roomList)=>{  
   const $board_list = document.getElementById("board-list");
   const $board_table = $board_list.querySelector(".board-table");
@@ -125,6 +134,7 @@ const updateArenaRoom = (roomList)=>{
     .then((res) => {
       currentNickname = res.data
     })
+    $tbody.removeEventListener("click", clickEventHandler);
     $tbody.addEventListener("click", (e) => {
       console.log(e.target.parentElement);
       if (e.target.className === `room-link room-${roomInfo.ROOM_NUMBER}`){
@@ -137,6 +147,7 @@ const updateArenaRoom = (roomList)=>{
 
 // 사용자 접속시 채팅방 리스트 최신화
 arenaSocket.on('updateRoomList', (roomList)=>{
+  
   updateArenaRoom(roomList)
 })
 
@@ -167,7 +178,6 @@ arenaSocket.on('countUpdate',(data)=>{
 
 
 // --------------------지훈 끝--------------------------------
-
 //
 $make_room_form.addEventListener("submit", handleRoomSubmit);
 
--- server.js
+++ server.js
@@ -113,34 +113,28 @@ app.use(
     console.log(`backSocket Event: ${event}`);
   });
 
-  socket.on("create_room", ({ room_name, chatRoomMethod, dev_lang,nickname }) => {
+  socket.on("create_room", ({ room_name, dev_lang,nickname }) => {
     console.log("create_room 이벤트 서버로 도착");
     console.log('닉네임확인',nickname)
-    if (chatRoomMethod === "one_to_one") {
-      chatRoomMethod = "1:1채팅";
-    } else {
-      chatRoomMethod = "오픈채팅";
-    }
-
     
     const roomInfo = {
       room_number: generateRoomNumber(),
       room_name: room_name,
-      chatRoomMethod: chatRoomMethod,
       dev_lang: dev_lang,
-      createdBy: socket.nickname,
+      createdBy: nickname,
       createdDate: new Date().toISOString().slice(0, 10),
     };
-
+    
     console.log("roomInfo", roomInfo);
-
+    
     rooms.set(room_name, roomInfo);
-
+    
     socket.emit("enter_room", {room_name, nickname, roomNum,})
- 
+    
     // 업데이트된 방 리스트 전체에 브로드캐스팅
     const updatedRoomList = Array.from(rooms.values());
     console.log("방목록 보여줘 : ", updatedRoomList);
+    
     ChatNamespace.emit("update_room_list", updatedRoomList);
   });
 
@@ -228,26 +222,20 @@ const countRoomUsers = (room_name) => {
     socket.emit('updateRoomList', roomList)
   })
 
-  socket.on("create_room", ({ room_name, chatRoomMethod, dev_lang,nickname }) => {
+  socket.on("create_room", ({ room_name, dev_lang,nickname }) => {
     console.log("create_room 이벤트 서버로 도착");
     // console.log("rooms : ", rooms);
     // console.log('닉넴',nickname)
 
-    if (chatRoomMethod === "one_to_one") {
-      chatRoomMethod = "1:1채팅";
-    } else {
-      chatRoomMethod = "오픈채팅";
-    }
 
     const roomInfo = {
       room_number: generateRoomNumber(),
       room_name: room_name,
-      chatRoomMethod: chatRoomMethod,
       dev_lang: dev_lang,
       createdBy: nickname ,
-      userCount : countRoomUsers(room_name) + 1,
+      userCount : countRoomUsers(room_name),
     };
-    // console.log(roomInfo);
+
     rooms.set(room_name, roomInfo);
     // 업데이트된 방 리스트 전체에 브로드캐스팅
     const updatedRoomList = Array.from(rooms.values());
