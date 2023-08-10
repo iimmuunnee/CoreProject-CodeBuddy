86b0ef88f8dc9e77c4abe2d4a34428229e0241a1
iimmuunnee <glgmsl@naver.com>
2023-08-10 17:30:43 +0900

    - 방 입장 시 안에서 방 제목 연동 (해결 - 방 안에서 퇴장 시 인원 수 감소 (해결 - 방 안에서 퇴장 문구 닉네임 연동 (해결 - 방 생성시 닉네임 리스트에 연동 (해결- 임휘훈

--- public/CodeArena/js/codeArenaList.js
+++ public/CodeArena/js/codeArenaList.js
@@ -90,6 +90,7 @@ const handleClick = (e) => {
   const target = e.target;
   if (target.classList.contains("room-link")) {
     const roomNumber = target.getAttribute("data-roomnumber");
+    const roomName = target.getAttribute("data-roomname") // 여기!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     if (roomNumber) {
       enterRoom(currentNickname, roomName, roomNumber);
     }
@@ -239,7 +240,6 @@ const enterRoom = (currentNickname, roomName, roomNum) => {
 const $leave_room = document.getElementById("leave_room");
 
 const leaveRoomBtn = () => {
-  let currentNickname
   console.log("leaveRoomBtn 함수 활성화");
   let page = document.getElementById("code_arena_zip");
   page.style.display = "none";                                                                                                                                                                                                                                                                                                                              
@@ -251,6 +251,7 @@ const leaveRoomBtn = () => {
   chat.style.display = "none";
   
   arenaSocket.emit("leave_room", {currentNickname});
+  arenaSocket.emit("leave_count")
 
 };
 arenaSocket.on('leaveuser',(data)=>{
